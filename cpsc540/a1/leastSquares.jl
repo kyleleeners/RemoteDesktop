1988066005e7fe271220ab67511635acc8754179
Kyle Leeners <kyle.jleeners@gmail.com>
2019-01-07 20:42:19 -0800

    540 a1 in prog

--- cpsc540/a1/leastSquares.jl
+++ cpsc540/a1/leastSquares.jl
@@ -1,4 +1,5 @@
 include("misc.jl")
+using LinearAlgebra
 
 function leastSquares(X,y)
 
@@ -12,6 +13,31 @@ function leastSquares(X,y)
 	# Make linear prediction function
 	predict(Xtilde) = [ones(size(Xtilde,1),1) Xtilde]*w
 
+	# Return model
+	return LinearModel(predict,w)
+end
+
+function leastSquaresRBFL2(X,y, σ, λ)
+    
+    # create rbf
+    rbf = rbfBasis(X, σ)
+    
+    # Add bias column
+	n = size(rbf,1)
+	Z = [ones(n,1) rbf]
+    
+    # Idenity * lambda
+    I = Diagonal(ones(n+1,n+1)) * λ
+    
+    # Find regression weights minimizing squared error
+    w = (Z'*Z - I)\(Z'*y)
+    
+    # Make linear prediction function
+    println(size(w))
+    println(size(Z))
+    
+    predict(Xtilde) = [ones(size(Xtilde,1),1) Xtilde]*w
+    
 	# Return model
 	return LinearModel(predict,w)
 end
